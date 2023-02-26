d930c2236ef4c6d9d82eed4fa6c3578a897b0d47
MiniGan067 <MiniGan067@yandex.ru>
2023-02-26 18:30:45 +0300

    update coursework

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,13 @@ import requests
 
 
 def get_data(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json(), "INFO: Данные получены!"
-    return None, f"WARNING: Статус ответа {response.status_code}"
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json(), "INFO: Данные получены!"
+
+        return None, f"WARNING: Статус ответа {response.status_code}"
+
+    except requests.exceptions.ConnectionError as e:
+        return None, "ERROR: requests.exceptions.ConnectionError"
